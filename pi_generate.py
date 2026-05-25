cede2108d730d4b873fe35778945fc1a8581a952
synthreason-source <georgewagenknecht1@gmail.com>
2026-05-26 08:10:08 +1000

    Update pi_generate.py

--- pi_generate.py
+++ pi_generate.py
@@ -1567,7 +1567,6 @@ class SentenceDatasetPreprocessor:
     def _tokenize_sentences(self, text: str) -> List[List[str]]:
         if self.lowercase:
             text = text.lower()
-        text = text.split()
 
         sentences: List[List[str]] = []
         current:   List[str]       = []
